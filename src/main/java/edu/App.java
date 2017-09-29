d3b1a74a8a5942f61cd1a2851128624a068e11ee
Kamil <km.cisak@gmail.com>
2017-09-29 22:10:57 +0200

    dalej w masterze

--- src/main/java/edu/App.java
+++ src/main/java/edu/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("siema kamil!");
+
+        System.out.println("dalej robie cos w masterze");
     }
 }
